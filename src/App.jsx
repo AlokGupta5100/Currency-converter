c15af978e1db0468cc139b15f8fbd38ba5e70cdb
Alok <alokgupta5100@gmail.com>
2023-12-29 21:57:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+function App(){
+  return (
+    <>
+      <h1 className='bg-orange-500 text-3xl py-3 justify-center'>
+        Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
